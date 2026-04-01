8aa649a26a8757ad2f9f7206ffa9084653447e88
prashantsaini-student <sainiprashant954857@gmail.com>
2026-04-01 11:21:36 +0530

    first commit

---
+++ react-ai-tool/src/App.jsx
@@ -0,0 +1,93 @@
+
+import { useState } from 'react'
+import './index.css'
+import { URL } from './constants';
+import Answer from './components/Answers';
+
+function App() {
+  const [question,setQuestion]=useState('');
+  const [result,setResult]=useState([])
+
+
+  
+  const payload ={
+    "contents": [{
+      "parts":[{"text": question}]        
+    }]
+  }
+  const askQuestion = async()=>{
+   
+    let response = await fetch(URL,{
+   
+    method:"POST",
+    body:JSON.stringify(payload)
+    
+ 
+  });
+
+  response = await response.json();
+  let dataString = response.candidates[0].content.parts[0].text;
+  dataString =dataString.split("*");
+  dataString=dataString.map((item)=>item.trim())
+
+  // console.log(dataString);
+  
+  setResult([...result,{type:'q',text:question},{type:'a',text:dataString}])
+}
+
+  console.log(result);
+  
+  return (
+    
+    <div className='grid grid-cols-5 h-screen text-center'>
+      <div className='col-span-1 bg-zinc-800'>
+
+      </div>
+      <div className='col-span-4 p-10'>
+        <div className='container h-110 overflow-auto'>
+        <div className='text-zinc-300'>
+          <ul>
+          {
+            result.map((item,index)=>(
+              <div key={index+Math.random()} className={item.type=='q'?'flex justify-end':''}>
+                {
+                item.type=='q'?  
+               <li key={index+Math.random()}
+               className='text-right p-4 border-8 bg-zinc-700 border-zinc-700 rounded-tl-3xl 
+               rounded-bl-3xl w-fit '><Answer ans={item.text} totalResult={1} index={index} type={item.type} /></li>
+               :item.text.map((ansItem,ansIndex)=>(
+                <li key={ansIndex+Math.random()} className='text-left '><Answer ans={ansItem} totalResult={item.length} type={item.type} index={ansIndex} /></li>
+
+               ))
+          }
+              </div>
+              
+            ))
+          }
+          </ul>
+          {/* <ul>
+        
+          {
+            result && result.map((item, index)=>(
+              <li key={index+Math.random()} className='text-left '><Answer ans={item} totalResult={result.length} index={index} /></li>
+
+            ))
+          }
+          </ul> */}
+          
+          
+        </div>
+        </div>
+       <div className='bg-zinc-800 w-1/2 p-1 pr-5 text-white m-auto rounded-4xl 
+       border border-zinc-700 flex h-16'>
+        <input type="text" value={question} onChange={(event)=>setQuestion(event.target.value)} className='w-full h-full p-3 outline-none' placeholder='Ask me Anything' />
+        <button onClick={askQuestion}>Ask</button>
+       </div>
+      </div>
+    </div>
+  )
+}
+
+
+
+export default App
---
+++ react-ai-tool/src/constants.js
@@ -0,0 +1,3 @@
+
+export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=AIzaSyClrlC_kZq24k0Mtp_ng3u1EcFjGQ1qtpI";
+
